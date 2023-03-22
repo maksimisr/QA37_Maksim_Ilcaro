25593381df40d4b5db1b861a5632a14ad920c0e0
maksimisr <maksim.israilov1996@gmail.com>
2023-03-22 21:23:45 +0200

    Homework$$

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -3,6 +3,10 @@ package manager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
@@ -35,5 +39,24 @@ click(By.xpath("//a[@href='/login?url=%2Fsearch']"));
     public boolean isLogged() {
 
         return isElementExists(By.xpath("//a[.=' Logout ']"));
+    }
+
+    public String getMessage(){
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.invisibilityOf(wd.findElement(By.cssSelector(".dialog-container"))));
+
+
+        return  wd.findElement(By.cssSelector(".dialog-container>2")).getText();
+    }
+
+
+
+    public void logout() {
+        click(By.xpath("//a[text()=' Logout ']"));
+    }
+
+    public boolean wrongLoginOrPassword(){
+
+        return  isElementExists(By.cssSelector("h2.message"));
     }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,9 +3,17 @@ package tests;
 
 import manager.HelperUser;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase{
+
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
     @Test
     public void loginSuccess(){
         app.getHelperUser().login();
@@ -13,5 +21,32 @@ public class LoginTest extends TestBase{
         app.getHelperUser().submitLogin();
 
         Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
+
+    public void loginSuccessModel(){
+        app.getHelperUser().login();
+        app.getHelperUser().fillLoginForm("maks-skam@mail.ru","Maksim1996$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
+    @Test
+    public void loginWrongEmail(){
+        app.getHelperUser().login();
+        app.getHelperUser().fillLoginForm("makskam@mail.ru","Maksim1996$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().wrongLoginOrPassword());
+    }
+
+    @Test
+    public void loginWrongPassword(){
+        app.getHelperUser().login();
+        app.getHelperUser().fillLoginForm("maks-skam@mail.ru","MakSDVZim1996$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().wrongLoginOrPassword());
     }
 }
